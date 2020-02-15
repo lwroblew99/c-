82dd04f97f3656ae38abbe9e52ab663443eff5db
Łucja Wróblewska <Lucja@MacBook-ucja.local>
2020-02-15 20:40:16 +0100

    tablice_dynamiczne

---
+++ cmake-build-debug/CMakeFiles/tablice_dynamiczne.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tablice_dynamiczne.dir/main.cpp.o"
+  "tablice_dynamiczne.pdb"
+  "tablice_dynamiczne"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tablice_dynamiczne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
